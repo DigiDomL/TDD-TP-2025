3ad1cd49ff5ee854735d30247caa8cc7609bb542
kylian <kykylh72@gmail.com>
2025-02-26 16:06:25 +0100

    Initial commit

---
+++ src/main/java/fr/formation/tddtp2025/TddTp2025Application.java
@@ -0,0 +1,13 @@
+package fr.formation.tddtp2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddTp2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddTp2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/formation/tddtp2025/TddTp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.tddtp2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTp2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
