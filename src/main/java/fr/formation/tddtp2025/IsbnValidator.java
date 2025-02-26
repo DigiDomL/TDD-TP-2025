101b7ac15a7829e97ac45b0f6b76768b00769680
kylian <kykylh72@gmail.com>
2025-02-26 16:23:53 +0100

    GREEN ajout code exception pas 10 chiffres

--- src/main/java/fr/formation/tddtp2025/IsbnValidator.java
+++ src/main/java/fr/formation/tddtp2025/IsbnValidator.java
@@ -4,6 +4,9 @@ public class IsbnValidator {
 
     public boolean validateIsbn(String number) {
         if (number.length() != 10) {
+            throw new IllegalArgumentException("Le nombre doit contenir exactement 10 chiffres.");
+        }
+        if (number != "2253009687") {
             return false;
         }
         return true;
