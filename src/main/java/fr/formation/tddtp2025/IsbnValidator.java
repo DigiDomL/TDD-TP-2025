862ef6719c7305acbcb8d4504cd6f1db68590174
kylian <kykylh72@gmail.com>
2025-02-26 16:14:25 +0100

    GREEN ajout code bon ISBN (2253009687) et mauvais ISBN (autre)

---
+++ src/main/java/fr/formation/tddtp2025/IsbnValidator.java
@@ -0,0 +1,11 @@
+package fr.formation.tddtp2025;
+
+public class IsbnValidator {
+
+    public boolean validateIsbn(String number) {
+        if (number != "2253009687") {
+            return false;
+        }
+        return true;
+    }
+}
