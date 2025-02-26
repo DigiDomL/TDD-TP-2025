55cf28d1204bbbe77fa08e8dd7abbd8056458928
kylian <kykylh72@gmail.com>
2025-02-26 16:18:28 +0100

    REFACTOR ajout code ISBN 10 chiffres

--- src/main/java/fr/formation/tddtp2025/IsbnValidator.java
+++ src/main/java/fr/formation/tddtp2025/IsbnValidator.java
@@ -3,7 +3,7 @@ package fr.formation.tddtp2025;
 public class IsbnValidator {
 
     public boolean validateIsbn(String number) {
-        if (number != "2253009687") {
+        if (number.length() != 10) {
             return false;
         }
         return true;
