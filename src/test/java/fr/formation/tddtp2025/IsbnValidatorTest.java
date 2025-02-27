0ea137bf57bcd9aa8490a07cf344fa062dd4e232
kylian <kykylh72@gmail.com>
2025-02-27 15:29:39 +0100

    Update coverage

--- src/test/java/fr/formation/tddtp2025/IsbnValidatorTest.java
+++ src/test/java/fr/formation/tddtp2025/IsbnValidatorTest.java
@@ -40,4 +40,25 @@ public class IsbnValidatorTest {
                 () -> IsbnValidator.validateIsbn("22530096877"));
         assertEquals("L'ISBN doit contenir exactement 10 ou 13 chiffres.", exception.getMessage());
     }
+
+    @Test
+    public void whenIsbn13IsValid_shouldReturnTrue() {
+        boolean result = IsbnValidator.validateIsbn("9780306406157");
+        assertTrue(result);
+    }
+
+    @Test
+    public void whenIsbn13IsInvalid_shouldReturnFalse() {
+        boolean result = IsbnValidator.validateIsbn("9780306406158");
+        assertFalse(result);
+    }
+
+    @Test
+    public void whenMultipleIsbn13IsValid_shouldReturnTrue() {
+        String[] validIsbns = {"9780306406157", "9781861972712"};
+        for (String isbn : validIsbns) {
+            boolean result = IsbnValidator.validateIsbn(isbn);
+            assertTrue(result);
+        }
+    }
 }
